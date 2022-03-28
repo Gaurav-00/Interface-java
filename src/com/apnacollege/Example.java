3c05e88c6edac06dd0816a295b821d79bfc97024
Gaurav-00 <shindegaurav5551@gmail.com>
2022-03-28 15:20:14 +0530

    Addition complete

--- src/com/apnacollege/Example.java
+++ src/com/apnacollege/Example.java
@@ -3,5 +3,9 @@ class Example{
     public static void main(String args[])
     {
         System.out.println("This is first java code!");
+        System.out.println("Hello");
+        Integer a=5,b=1,c;
+        c=a+b;
+        System.out.println(c);
     }
 }
